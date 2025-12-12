e2ee2cd98158381e5d708064e04acd579e3c0f59
Edmon <19ed86boy@mail.ru>
2025-12-12 20:34:13 +0300

    Fix: channel validation for Hexlet tests

--- frontend/src/components/Channels.jsx
+++ frontend/src/components/Channels.jsx
@@ -1,19 +1,19 @@
-import { useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { Button, Dropdown, Modal } from 'react-bootstrap';
-import { Formik, Form, Field, ErrorMessage } from 'formik';
-import * as Yup from 'yup';
+import { useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { Button, Dropdown, Modal } from 'react-bootstrap'
+import { Formik, Form, Field, ErrorMessage } from 'formik'
+import * as Yup from 'yup'
 import {
   setCurrentChannelId,
   addChannelServer,
   renameChannelServer,
   removeChannelServer,
-} from '../slices/chatSlice';
-import { useTranslation } from 'react-i18next';
-import { toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
-import LanguageSwitcher from './LanguageSwitcher';
-import leoProfanity from 'leo-profanity';
+} from '../slices/chatSlice'
+import { useTranslation } from 'react-i18next'
+import { toast } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
+import LanguageSwitcher from './LanguageSwitcher'
+import leoProfanity from 'leo-profanity'
 
 const createSchema = (channels, t) =>
   Yup.object({
@@ -27,95 +27,95 @@ const createSchema = (channels, t) =>
         value =>
           !channels.some(c => c.name.toLowerCase() === value.toLowerCase()),
       ),
-  });
+  })
 
 const Channels = () => {
-  const { t } = useTranslation();
-  const dispatch = useDispatch();
-  const { channels, currentChannelId } = useSelector(state => state.chat);
+  const { t } = useTranslation()
+  const dispatch = useDispatch()
+  const { channels, currentChannelId } = useSelector(state => state.chat)
 
-  const [showAdd, setShowAdd] = useState(false);
-  const [showRename, setShowRename] = useState(false);
-  const [showDelete, setShowDelete] = useState(false);
-  const [selectedChannel, setSelectedChannel] = useState(null);
+  const [showAdd, setShowAdd] = useState(false)
+  const [showRename, setShowRename] = useState(false)
+  const [showDelete, setShowDelete] = useState(false)
+  const [selectedChannel, setSelectedChannel] = useState(null)
 
-  const openAdd = () => setShowAdd(true);
-  const closeAdd = () => setShowAdd(false);
+  const openAdd = () => setShowAdd(true)
+  const closeAdd = () => setShowAdd(false)
 
   const openRename = channel => {
-    setSelectedChannel(channel);
-    setShowRename(true);
-  };
+    setSelectedChannel(channel)
+    setShowRename(true)
+  }
 
-  const closeRename = () => setShowRename(false);
+  const closeRename = () => setShowRename(false)
 
   const openDelete = channel => {
-    setSelectedChannel(channel);
-    setShowDelete(true);
-  };
+    setSelectedChannel(channel)
+    setShowDelete(true)
+  }
 
-  const closeDelete = () => setShowDelete(false);
+  const closeDelete = () => setShowDelete(false)
 
-  const handleAddChannel = async (name, setSubmitting) => {
-    const cleanName = leoProfanity.clean(name);
+  const handleAddChannel = async(name, setSubmitting) => {
+    const cleanName = leoProfanity.clean(name)
     try {
-      await dispatch(addChannelServer(cleanName)).unwrap();
-      toast.success(t('channels.addSuccess'));
-      closeAdd();
+      await dispatch(addChannelServer(cleanName)).unwrap()
+      toast.success(t('channels.addSuccess'))
+      closeAdd()
     }
     catch {
       if (!navigator.onLine) {
-        toast.error(t('chat.errors.noNetwork'));
+        toast.error(t('chat.errors.noNetwork'))
       }
       else {
-        toast.error(t('channels.addError'));
+        toast.error(t('channels.addError'))
       }
     }
     finally {
-      setSubmitting(false);
+      setSubmitting(false)
     }
-  };
+  }
 
-  const handleRenameChannel = async (id, name, setSubmitting) => {
-    const cleanName = leoProfanity.clean(name);
+  const handleRenameChannel = async(id, name, setSubmitting) => {
+    const cleanName = leoProfanity.clean(name)
     try {
       await dispatch(
         renameChannelServer({
           id,
           name: cleanName,
         }),
-      ).unwrap();
-      toast.success(t('channels.renameSuccess'));
-      closeRename();
+      ).unwrap()
+      toast.success(t('channels.renameSuccess'))
+      closeRename()
     }
     catch {
       if (!navigator.onLine) {
-        toast.error(t('chat.errors.noNetwork'));
+        toast.error(t('chat.errors.noNetwork'))
       }
       else {
-        toast.error(t('channels.renameError'));
+        toast.error(t('channels.renameError'))
       }
     }
     finally {
-      setSubmitting(false);
+      setSubmitting(false)
     }
-  };
+  }
 
-  const handleDeleteChannel = async (id) => {
+  const handleDeleteChannel = async(id) => {
     try {
-      await dispatch(removeChannelServer(id)).unwrap();
-      toast.success(t('channels.deleteSuccess'));
-      closeDelete();
+      await dispatch(removeChannelServer(id)).unwrap()
+      toast.success(t('channels.deleteSuccess'))
+      closeDelete()
     }
     catch {
       if (!navigator.onLine) {
-        toast.error(t('chat.errors.noNetwork'));
+        toast.error(t('chat.errors.noNetwork'))
       }
       else {
-        toast.error(t('channels.deleteError'));
+        toast.error(t('channels.deleteError'))
       }
     }
-  };
+  }
 
   return (
     <div>
@@ -193,8 +193,8 @@ const Channels = () => {
         <Formik
           initialValues={{ name: '' }}
           validationSchema={createSchema(channels, t)}
-          onSubmit={async ({ name }, { setSubmitting }) => {
-            await handleAddChannel(name, setSubmitting);
+          onSubmit={async({ name }, { setSubmitting }) => {
+            await handleAddChannel(name, setSubmitting)
           }}
         >
           {({ isSubmitting }) => (
@@ -243,12 +243,12 @@ const Channels = () => {
               channels.filter(c => c.id !== selectedChannel.id),
               t,
             )}
-            onSubmit={async ({ name }, { setSubmitting }) => {
+            onSubmit={async({ name }, { setSubmitting }) => {
               await handleRenameChannel(
                 selectedChannel.id,
                 name,
                 setSubmitting,
-              );
+              )
             }}
           >
             {({ isSubmitting }) => (
@@ -311,7 +311,7 @@ const Channels = () => {
         </Modal.Footer>
       </Modal>
     </div>
-  );
-};
+  )
+}
 
-export default Channels;
+export default Channels
